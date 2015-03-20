c5b2e87c8d3a8fb36906a742f0c03cf88063121d
mayuresh <mayuresh@cs.duke.edu>
2015-03-20 15:19:12 -0400

    removed password

--- src/main/java/edu/duke/dbmsplus/datahooks/conf/StarfishDatabaseCredentials.java
+++ src/main/java/edu/duke/dbmsplus/datahooks/conf/StarfishDatabaseCredentials.java
@@ -8,5 +8,5 @@ package edu.duke.dbmsplus.datahooks.conf;
 public class StarfishDatabaseCredentials {
 	public static String CONNECTION_STRING = "jdbc:mysql://inw-10.rfiserve.net:3306/starfish_production";
 	public static String USERNAME = "rfi_test";
-	public static String PASSWORD = "90f55b33e";
+	public static String PASSWORD = "";
 }
